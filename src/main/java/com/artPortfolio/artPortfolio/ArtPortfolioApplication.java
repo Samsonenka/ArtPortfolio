f565048de9baffc9b4cdf2e7259bd5d9bb9d80d0
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-09-22 16:49:39 +0200

    first commit

---
+++ src/main/java/com/artPortfolio/artPortfolio/ArtPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.artPortfolio.artPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtPortfolioApplication.class, args);
+	}
+
+}
